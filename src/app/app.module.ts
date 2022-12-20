1086eedef8c9793e7aeac40d235c37e71061848c
Michael Lauderback <mllauderback@gmail.com>
2022-12-20 00:21:54 -0600

    Rudimentary file menubar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,11 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './store/reducers';
 import { MenubarComponent } from './components/menubar/menubar.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { MenuitemComponent } from './components/menuitem/menuitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenubarComponent,
-    MenuComponent,
-    MenuitemComponent
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class MenubarComponent {
 
+  public acknowledge(): void {
+    console.log('menu item clicked');
+  }
+
 }
